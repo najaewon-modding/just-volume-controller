c00f4987d93ca657aeb0b857c0add08e6ac90b60
JWN <89252111+skwodnjs@users.noreply.github.com>
2026-09-05 05:55:26 +0900

    Set up client-only mod foundation

--- src/main/java/net/njw/volumedesk/VolumeDesk.java
+++ src/main/java/net/njw/volumedesk/VolumeDesk.java
@@ -1,18 +1,9 @@
 package net.njw.volumedesk;
 
-import org.slf4j.Logger;
-
-import com.mojang.logging.LogUtils;
-
-import net.neoforged.bus.api.IEventBus;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.ModContainer;
 
-@Mod(VolumeDesk.MODID)
-public class VolumeDesk {
-    public static final String MODID = "njw_volume_desk";
-    public static final Logger LOGGER = LogUtils.getLogger();
-
-    public VolumeDesk(IEventBus modEventBus, ModContainer modContainer) {
-    }
+@Mod(value = VolumeDesk.MOD_ID, dist = Dist.CLIENT)
+public final class VolumeDesk {
+    public static final String MOD_ID = "njw_volume_desk";
 }
